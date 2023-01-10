9ca893639d76a0e9a825171c768ffcc5b487f234
Karol Meksuła <karol.meksula@fenige.pl>
2023-01-10 14:42:07 +0100

    Project initialized

---
+++ src/main/java/com/meksula/analyzr/TextAnalyzrApplication.java
@@ -0,0 +1,13 @@
+package com.meksula.analyzr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnalyzrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meksula/analyzr/TextAnalyzrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meksula.analyzr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalyzrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
